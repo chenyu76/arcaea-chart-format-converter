8bf722853a68376c6f581a1c923c512397c6584f
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2020-11-22 17:02:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,6 +28,12 @@ class _aff_file
         void tap_arctap();
         void tap_skyline();
         void arctap_skyline();
+        void remove_(string);
+        void add_a_note();
+
+        void set_convertd_0();
+
+        void mix_chart(string filename = "1.aff");
 
         void export_aff(string filename = "a.aff");
 
@@ -40,16 +46,18 @@ class _aff_file
         vector<command> commands;
 };
 
+string get_filename(string);
+
 int main()
 {
     system("chcp 65001");
+    system("color fc");
     system("cls");
+
+
+    cout << "读取文件" << endl;
     string filename = "2.aff";
-    string str;
-    cout << "输入读取文件名(留空将读取当前目录下的2.aff):";
-    getline(cin, str);
-    if(str != "")
-        filename = str;
+    filename = get_filename(filename);
     _aff_file aff_file(filename);
     string choose = "1";
     while(true)
@@ -57,16 +65,19 @@ int main()
         system("cls");
         cout << "读取了文件" << filename << endl
             << "你想做什么？" << endl
+            << "x. 不保存退出" << endl
             << "0. 退出并保存" << endl
             << "1. 全部转换成skyline" << endl
             << "2. 全部转换成arc" << endl
+            << "3. 混合另一个谱面" << endl
             << "----以下是杂项----" << endl
             << "A. 将arc转成skyline" << endl
             << "B. 将skyline转成arc" << endl
             << "C. 将地面note转成skyline" << endl
             << "D. 将地面hold转成skyline" << endl
             << "E. 将天空note转成skyline" << endl
-            << "F. 在歌曲最前面添加一个note" << endl;
+            << "F. 在歌曲最前面添加一个note" << endl
+            //<< "G. 删除指定类型的元素" << endl
             << " " << endl;
 
 
@@ -74,14 +85,16 @@ int main()
         getline(cin, choose);
         switch(choose[0])
         {
+        case 'x':
+            {
+                return 0;
+            }
         case '0':
             {
-                cout << "输入保存文件名(留空将覆盖原文件):";
-                getline(cin, str);
-                if(str == "")
-                    aff_file.export_aff(filename);
-                else
-                    aff_file.export_aff(str);
+                cout << "保存文件" << endl;
+                filename = get_filename(filename);
+                aff_file.export_aff(filename);
+                cout << "文件保存到了" << filename << endl;
                 system("pause");
                 return 0;
             }
@@ -91,15 +104,34 @@ int main()
                 aff_file.hold_skyline();
                 aff_file.tap_skyline();
                 aff_file.arctap_skyline();
+                cout << "注意：现在谱面里的物量为零," << endl
+                    << "你可能需要使用F选项添加一个" << endl;
                 break;
             }
         case '2':
             {
-                aff_file.arc_skyline();
+                //aff_file.arc_skyline();
                 aff_file.hold_skyline();
                 aff_file.tap_skyline();
                 aff_file.arctap_skyline();
-                aff_file.skyline_arc();
+                aff_file.set_convertd_0();
+                cout << "生成为什么颜色的arc？" << endl
+                    << "0. 全是蓝的" << endl
+                    << "1. 全是红的" << endl
+                    << "2. 全是绿的" << endl
+                    << "A. 自动选择" << endl
+                    << "输入选择：";
+                string color_choose;
+                getline(cin, color_choose);
+
+                aff_file.skyline_arc(color_choose[0]);
+                break;
+            }
+        case '3':
+            {
+                string mix_filename = "1.aff";
+                mix_filename = get_filename(mix_filename);
+                aff_file.mix_chart(mix_filename);
                 break;
             }
         case 'A':
@@ -115,7 +147,7 @@ int main()
                     << "0. 全是蓝的" << endl
                     << "1. 全是红的" << endl
                     << "2. 全是绿的" << endl
-                    << "或者按下回车自动选择" << endl
+                    << "A. 自动选择" << endl
                     << "输入选择：";
                 string color_choose;
                 getline(cin, color_choose);
@@ -144,8 +176,17 @@ int main()
         case 'F':
         case 'f':
             {
-                cout << "没做" << endl;
+                aff_file.add_a_note();
                 break;
+            }
+        case 'G':
+        case 'g':
+            {
+                cout << "删除什么？" << endl
+                    << "可选项: note, hold, arctap, skyline, arc" << endl
+                    << "输入你的选择";
+                getline(cin, choose);
+                aff_file.remove_(choose);
             }
         default:
             cout << "错误的选择，请重试" << endl;
@@ -154,6 +195,20 @@ int main()
         getline(cin, choose);
     }
 }
+
+
+string get_filename(string default_filename)
+{
+    string str;
+    cout << "输入文件名(默认为" << default_filename << "):";
+    getline(cin, str);
+    if(str[0] == '"')
+        str = str.substr(1, str.size() - 2);
+    if(str != "")
+        return str;
+    return default_filename;
+}
+
 
 
 _aff_file::_aff_file(string filename)
@@ -165,6 +220,7 @@ _aff_file::_aff_file(string filename)
     if (!infile.is_open())
     {
         cout << "文件打开出错，请检查文件是否存在" << endl;;
+        system("pause");
         exit (1);
     }
 
@@ -261,6 +317,37 @@ _aff_file::~_aff_file()
     //dtor
 }
 
+
+void _aff_file::set_convertd_0()
+{
+    for(int i = 0; i < commands.size(); i++)
+    {
+        commands[i].converted = 0;
+    }
+}
+
+void _aff_file::mix_chart(string filename)
+{
+    _aff_file tmp_aff_file(filename);
+    for(int i = 0; i < tmp_aff_file.commands.size(); i++)
+    {
+        this->commands.push_back(tmp_aff_file.commands[i]);
+    }
+
+}
+
+
+void _aff_file::add_a_note()
+{
+    command tmp_cmd;
+    tmp_cmd.name = "tap";
+    tmp_cmd.converted = true;
+    tmp_cmd.parameter.push_back("0");
+    tmp_cmd.parameter.push_back("1");
+    commands.push_back(tmp_cmd);
+}
+
+
 void _aff_file::arc_skyline()
 {
     for(int i = 0; i < this->commands.size(); i++)
@@ -270,6 +357,7 @@ void _aff_file::arc_skyline()
             if(this->commands[i].name == "arc" && this->commands[i].parameter[9] == "false")
             {
                 this->commands[i].parameter[9] = "true";
+                this->commands[i].converted = true;
             }
         }
     }
@@ -307,6 +395,7 @@ void _aff_file::skyline_arc(char color)
 
                 }
                 this->commands[i].arctap.clear();
+                this->commands[i].converted = true;
             }
         }
     }
@@ -344,6 +433,7 @@ void _aff_file::hold_skyline()
                 for(int j = 0; j < 6; j++)
                     this->commands[i].parameter.push_back(param[j]);
                 this->commands[i].name = "arc";
+                this->commands[i].converted = true;
             }
         }
     }
@@ -382,6 +472,7 @@ void _aff_file::tap_skyline()
                 for(int j = 0; j < 6; j++)
                     this->commands[i].parameter.push_back(param[j]);
                 this->commands[i].name = "arc";
+                this->commands[i].converted = true;
             }
         }
     }
@@ -434,6 +525,7 @@ void _aff_file::arctap_skyline()
                     tmp_cmd.converted = true;
 
                     this->commands[i].arctap.clear();
+                    this->commands[i].converted = true;
 
                     i += 1;
                     this->commands.insert(this->commands.begin()+i, tmp_cmd);
@@ -447,6 +539,55 @@ void _aff_file::arctap_skyline()
     }
 }
 
+
+void _aff_file::remove_(string type)
+{
+    if(type == "arc")
+    {
+        for(int i = 0; i < commands.size(); i++)
+        {
+            if(commands[i].name == "arc")
+                if(commands[i].parameter[9] == "false")
+                    commands.erase(commands.begin() + i);
+        }
+    }
+    else if(type == "skyline")
+    {
+        for(int i = 0; i < commands.size(); i++)
+        {
+            if(commands[i].name == "arc")
+                if(commands[i].parameter[9] == "true")
+                    commands.erase(commands.begin() + i);
+        }
+    }
+    else if(type == "hold")
+    {
+        for(int i = 0; i < commands.size(); i++)
+        {
+            if(commands[i].name == "hold")
+                commands.erase(commands.begin() + i);
+        }
+    }
+    else if(type == "note")
+    {
+        for(int i = 0; i < commands.size(); i++)
+        {
+            if(commands[i].name == "tap")
+                commands.erase(commands.begin() + i);
+        }
+    }
+    else if(type == "arctap")
+    {
+        for(int i = 0; i < commands.size(); i++)
+        {
+            commands[i].arctap.clear();
+        }
+    }
+    else
+    {
+        cout << "错误的输入！" << endl;
+    }
+}
 
 void _aff_file::export_aff(string filename)
 {
